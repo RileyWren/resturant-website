8326556b285a4913cd37a04b6082b9b80340a81a
xcx-7 <zainab.42847@gmail.com>
2025-05-31 07:56:53 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Hero from './components/Hero'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    
+      <Hero />
+    
+    </>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,17 @@
 
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-import img1 from './assets/1.jpg';
-import img2 from './assets/2.jpg';
-import img3 from './assets/3.jpg';
+
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
 
 const images = [img1, img2, img3];
 
-function App() {
+function Hero() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -42,20 +45,16 @@ function App() {
       </AnimatePresence>
 
       {/* Navigation buttons */}
-      <button
-        onClick={prevImage}
-        className="absolute left-4 top-1/2 -translate-y-1/2 text-white text-4xl z-10"
-      >
-        &lt;
-      </button>
-      <button
-        onClick={nextImage}
-        className="absolute right-4 top-1/2 -translate-y-1/2 text-white text-4xl z-10"
-      >
-        &gt;
-      </button>
+     <FontAwesomeIcon icon={faArrowLeft}
+  onClick={prevImage}
+  className="absolute left-4 top-1/2 -translate-y-1/2 text-white text-4xl z-10 cursor-pointer" />
+
+<FontAwesomeIcon icon={faArrowRight}
+  onClick={nextImage}
+  className="absolute right-4 top-1/2 -translate-y-1/2 text-white text-4xl z-10 cursor-pointer" />
+
     </div>
   );
 }
 
-export default App;
+export default Hero;
