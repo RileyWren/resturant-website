52f1bffe10bef53375bb3f4cd3e7763a161f82d1
xcx-7 <zainab.42847@gmail.com>
2025-06-07 09:45:33 +0600

    updated

--- src/components/SignatureDish.jsx
+++ src/components/SignatureDish.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import coffee from '../assets/11.jpeg'
+import noodles from '../assets/noodles2.jpeg'
+import soupNoodles from '../assets/soupNoodles.jpeg'
+
 
 const SignatureDish = () => {
   return (
     <div>
         <h2>Explore by Our Cuisines</h2>
-        <div></div>
+        <div>
+            <img src={coffee} />
+            <img src={noodles} />
+            <img src={soupNoodles} />
+
+        </div>
     </div>
   )
 }
