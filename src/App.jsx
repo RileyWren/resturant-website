4e3464568225b12c4c685369fe31f239ae6dac67
xcx-7 <zainab.42847@gmail.com>
2025-06-08 20:43:04 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
-import Hero from './components/Hero'
-import SecondPart from './components/SecondPart'
-import SignatureDish from './components/SignatureDish'
-import Appetizer from './components/Appetizer'
-import MainDish from './components/MainDish'
-import ExploreMenu from './components/ExploreMenu'
+// import React from 'react'
+// import Hero from './components/Hero'
+// import SecondPart from './components/SecondPart'
+// import SignatureDish from './components/SignatureDish'
+// import Appetizer from './components/Appetizer'
+// import MainDish from './components/MainDish'
+// import ExploreMenu from './components/ExploreMenu'
 
 
-const App = () => {
-  return (
-    <div className='bg-neutral-950 overflow-x-hidden'>
+// const App = () => {
+//   return (
+//     <div className='bg-neutral-950 overflow-x-hidden'>
     
-      <Hero />
-      <SecondPart />
-    <SignatureDish />
-    <Appetizer />
-    <MainDish />
-    <ExploreMenu />
-    </div>
-  )
-}
+//       <Hero />
+//       <SecondPart />
+//     <SignatureDish />
+//     <Appetizer />
+//     <MainDish />
+//     <ExploreMenu />
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
+
+
+
+// main.jsx or index.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import MenuPage from './pages/MenuPage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/menu" element={<MenuPage />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+)
--- src/components/ExploreMenu.jsx
+++ src/components/ExploreMenu.jsx
@@ -1,3 +1,32 @@
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import { ArrowRight } from 'lucide-react'
+
+// const ExploreMenu = () => {
+//   const navigate = useNavigate()
+
+//   const handleClick = () => {
+//     navigate('/menu') // this assumes you have a route set for /menu
+//   }
+
+//   return (
+//     <div className='bg-neutral-800 h-50 w-screen flex flex-col items-center justify-center p-8 text-white'>
+//       <h2 className='text-2xl font-bold mb-4'>Crafting Memorable Experiences</h2>
+//       <button 
+//         onClick={handleClick} 
+//         className='flex items-center gap-2 bg-white text-neutral-800 px-4 py-2 rounded hover:bg-neutral-300 transition'
+//       >
+//         Explore Menu
+//         <ArrowRight size={20} />
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default ExploreMenu
+
+
+
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ArrowRight } from 'lucide-react'
