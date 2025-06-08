1d8f8440d0945a8c21c588f9f2563dcbb2f99f3e
xcx-7 <zainab.42847@gmail.com>
2025-06-08 22:06:01 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,21 +24,35 @@
 // export default App
 
 
-
-// main.jsx or index.jsx
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import MenuPage from './pages/MenuPage'
+import { Routes, Route } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/menu" element={<MenuPage />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
+import Hero from './components/Hero'
+import SecondPart from './components/SecondPart'
+import SignatureDish from './components/SignatureDish'
+import Appetizer from './components/Appetizer'
+import MainDish from './components/MainDish'
+import ExploreMenu from './components/ExploreMenu'
+import MenuPage from './pages/MenuPage' // <-- create this page
+
+const Home = () => (
+  <div className='bg-neutral-950 overflow-x-hidden'>
+    <Hero />
+    <SecondPart />
+    <SignatureDish />
+    <Appetizer />
+    <MainDish />
+    <ExploreMenu />
+  </div>
 )
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<MenuPage />} />
+    </Routes>
+  )
+}
+
+export default App
